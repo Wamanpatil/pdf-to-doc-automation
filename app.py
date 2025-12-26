a17e12c9a4986ecb2a230798b20e7478e284d983
Waman Patil <patilwaman471@gmail.com>
2025-12-26 20:53:39 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -77,5 +77,7 @@ def generate():
     generate_doc()
     return send_file(OUTPUT_FILE, as_attachment=True)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
